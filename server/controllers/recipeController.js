cb49b4ffecddfa4f4560214ac28d245b9f2ba624
periakteon <masumgokyuz@gmail.com>
2023-03-27 03:56:26 +0300

    recipe controller and index.ejs developed

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -12,30 +12,40 @@ const Recipe = require("../models/Recipe");
 // aşağıdaki asenkron fonksiyonu "homepage" olarak export ediyoruz, recipeRoutes.js'te kullanmak için
 exports.homepage = async (req, res) => {
   try {
-
     // 5 kategori gösterilmesini istediğimiz için limit koyuyoruz
     const limitNumber = 5;
     const categories = await Category.find({}).limit(limitNumber);
+
+    // "-1" yazarak aslında descending order yapmış oluyoruz, yani tersten sıralıyoruz
+    const latest = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+    // Anasayfadaki "Thai Recipes" kısmı için
+    const thai = await Recipe.find({"category": "Thai"}).limit(limitNumber);
+    // Anasayfadaki "American Recipes" kısmı için
+    const american = await Recipe.find({"category": "American"}).limit(limitNumber);
+    // Anasayfadaki "Mexican Recipes" kısmı için
+    const chinese = await Recipe.find({"category": "Chinese"}).limit(limitNumber);
+
+    //"index.ejs"te dot notation ile erişebilmek için "latest", "thai", "american" ve "chinese" değişkenlerini bir nesne olarak "food"da saklıyoruz
+    const food = { latest, thai, american, chinese };
 
 
     // views'teki "index.ejs" dosyasını göstermesini istiyoruz
     // server.js'te "layout"ı "./layouts/main" olarak belirlemiştik; dolayısıyla, main.ejs'te kullanılmak ve index'te gösterilmek üzere bir "title" nesnesi gönderiyoruz.
     // çünkü main.ejs'te şöyle bir kodumuz var:     <%= typeof title != "undefined" ? title: "" %>
-    // veritabanından aldığımız kategorileri de JSON şeklinde göndermek ve anasayfada görüntülemek için (yani, view engine'de kullanmak için) onu da render ediyoruz
-    res.render("index", { title: "Recipe Blog - Home", categories });
+    // veritabanından aldığımız kategorileri ve son eklenenleri de JSON şeklinde göndermek ve anasayfada görüntülemek için (yani, view engine'de kullanmak için) onu da render ediyoruz
+    res.render("index", { title: "Recipe Blog - Home", categories, food });
   } catch (error) {
     res.status(500).send({ message: error || "Homepage'de hata oluştu" });
   }
   console.log(" '/' adresine GET isteği geldi");
 };
 
-
-/** 
- * 
+/**
+ *
  * GET /categories
  * Categories
- * 
-*/
+ *
+ */
 exports.exploreCategories = async (req, res) => {
   try {
     const limitNumber = 20;
@@ -47,12 +57,11 @@ exports.exploreCategories = async (req, res) => {
   console.log(" '/categories' adresine GET isteği geldi");
 };
 
-// DUMMY RECIPE EKLEMEK İÇİN
 // async function dummyRecipe() {
 //   try {
 //     await Recipe.insertMany([
 //     {
-//       "name": "Pilav üstü kuru",
+//       "name": "Çince",
 //       "description": "Klasik türk yemeği",
 //       "email": "masumgokyuz@gmail.com",
 //       "ingredients": [
@@ -62,11 +71,11 @@ exports.exploreCategories = async (req, res) => {
 //         "ekmek",
 //         "fasulye"
 //       ],
-//       "category": "Thai",
+//       "category": "Chinese",
 //       "image": "stir-fried-vegetables.jpg",
 //     },
 //     {
-//       "name": "Pilav üstü qwefqwefqw",
+//       "name": "Pamerikan",
 //       "description": "Klasik türk 124",
 //       "email": "masumgokyuz@gmail.com",
 //       "ingredients": [
@@ -80,7 +89,7 @@ exports.exploreCategories = async (req, res) => {
 //       "image": "stir-fried-vegetables.jpg",
 //     },
 //     {
-//       "name": "Pilav üstü33333 qwefqwefqw",
+//       "name": "Thaii",
 //       "description": "Klasik t3ürk 124",
 //       "email": "masumgokyuz@gmail.com",
 //       "ingredients": [
@@ -90,7 +99,7 @@ exports.exploreCategories = async (req, res) => {
 //         "ekmek",
 //         "fasulye"
 //       ],
-//       "category": "Mexican",
+//       "category": "Thai",
 //       "image": "stir-fried-vegetables.jpg",
 //     }
 //     ])
@@ -98,5 +107,5 @@ exports.exploreCategories = async (req, res) => {
 //     console.error({ message: error || "Tarif eklenirken hata oluştu" });
 //   }
 // }
-//
+
 // dummyRecipe();
